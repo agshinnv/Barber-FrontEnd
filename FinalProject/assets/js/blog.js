6e64ee7823edad0c121cdc99e370839ac8eb329f
Agshin N. Valiyev <agshinnv@code.edu.az>
2024-07-23 01:49:39 +0400

    team detail, blog, post pages added

---
+++ FinalProject/assets/js/blog.js
@@ -0,0 +1,259 @@
+
+
+$(function() {
+
+        var currentPage = 1;
+        var totalPages = $('.page-link').length;
+
+        function loadPage(page) {
+            // Logic to load content for the specified page
+            $('#content').text('Content for page ' + page);
+            $('.page-link').removeClass('active');
+            $('.page-link').eq(page - 1).addClass('active');
+        }
+
+        $('.page-link').click(function(e) {
+            e.preventDefault();
+            currentPage = $(this).text();
+            loadPage(currentPage);
+        });
+
+        $('.prev').click(function(e) {
+            e.preventDefault();
+            if (currentPage > 1) {
+                currentPage--;
+                loadPage(currentPage);
+            }
+        });
+
+        $('.next').click(function(e) {
+            e.preventDefault();
+            if (currentPage < totalPages) {
+                currentPage++;
+                loadPage(currentPage);
+            }
+        });
+
+    //#region OnTop
+    var progressPath = document.querySelector('.progress-wrap path');
+    var pathLength = progressPath.getTotalLength();
+    progressPath.style.transition = progressPath.style.WebkitTransition = 'none';
+    progressPath.style.strokeDasharray = pathLength + ' ' + pathLength;
+    progressPath.style.strokeDashoffset = pathLength;
+    progressPath.getBoundingClientRect();
+    progressPath.style.transition = progressPath.style.WebkitTransition = 'stroke-dashoffset 10ms linear';
+    var updateProgress = function () {
+        var scroll = $(window).scrollTop();
+        var height = $(document).height() - $(window).height();
+        var progress = pathLength - (scroll * pathLength / height);
+        progressPath.style.strokeDashoffset = progress;
+    }
+    updateProgress();
+    $(window).scroll(updateProgress);
+    var offset = 150;
+    var duration = 550;
+    jQuery(window).on('scroll', function () {
+        if (jQuery(this).scrollTop() > offset) {
+            jQuery('.progress-wrap').addClass('active-progress');
+        } else {
+            jQuery('.progress-wrap').removeClass('active-progress');
+        }
+    });
+    jQuery('.progress-wrap').on('click', function (event) {
+        event.preventDefault();
+        jQuery('html, body').animate({
+            scrollTop: 0
+        }, duration);
+        return false;
+    })
+    //#endregion
+
+
+
+    //#region Slider
+    $('#kenburnsSliderContainer').vegas({
+        slides: [{
+            src: "assets/images/slider/1.jpg"
+        }, {
+            src: "assets/images/slider/2.jpg"
+        }, {
+            src: "assets/images/slider/3.jpg"
+        }, {
+            src: "assets/images/slider/4.jpg"
+        }],
+        overlay: false,
+        transition: 'fade3',
+        animation: 'kenburnsUpRight',
+        transitionDuration: 1000,
+        delay: 10000,
+        animationDuration: 20000
+    });
+    //#endregion
+
+
+    //#region HeaderScroll
+    $(window).on("scroll", function () {
+        var bodyScroll = $(this).scrollTop(),
+          navbar = $(".navbar");
+        if (bodyScroll > 100) {
+          navbar.addClass("nav-scroll");
+        } else {
+          navbar.removeClass("nav-scroll");
+        }
+      })
+    //#endregion
+
+    
+
+    //#region Scrollit    
+    $.scrollIt({
+        upKey: 38, // key code to navigate to the next section
+        downKey: 40, // key code to navigate to the previous section
+        easing: 'swing', // the easing function for animation
+        scrollTime: 600, // how long (in ms) the animation takes
+        activeClass: 'active', // class given to the active nav element
+        onPageChange: null, // function(pageIndex) that is called when page is changed
+        topOffset: -70 // offste (in px) for fixed top navigation
+    })
+    //#endregion
+  
+
+
+    //#region Testimonials
+    $('.content .owl-carousel').owlCarousel({
+        loop: true,
+        margin: 30,
+        mouseDrag: true,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        dots: false,
+        nav: false,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    });
+    //#endregion
+
+
+
+    //#region Features
+    $('#features .owl-carousel').owlCarousel({
+        loop: true,
+        margin: 30,
+        mouseDrag: true,
+        autoplay: false,
+        dots: true,
+        autoplayHoverPause: true,
+        nav: false,
+        navText: ["<span class='lnr ti-angle-left'></span>", "<span class='lnr ti-angle-right'></span>"],
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            }
+        }
+    });
+    //#endregion
+
+    //#region Video
+    // $("a.video").YouTubePopUp();
+    //#endregion
+
+
+    //#region TeamSlider
+    $('#team .owl-carousel').owlCarousel({
+        loop: true,
+        margin: 30,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        dots: false,
+        mouseDrag: true,
+
+        nav: false,
+        navText: ["<span class='lnr ti-angle-left'></span>", "<span class='lnr ti-angle-right'></span>"],
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            }
+        }
+    });
+    //#endregion
+
+
+
+    //#region News&Blog
+    $('#news .owl-carousel').owlCarousel({
+        loop: true,
+        margin: 30,
+        mouseDrag: true,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        dots: true,
+        nav: false,
+        navText: ["<span class='lnr ti-angle-left'></span>", "<span class='lnr ti-angle-right'></span>"],
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 2
+            }
+        }
+    });
+    //#endregion
+    
+
+    //#region Colleagues
+    $('#colleagues .owl-carousel').owlCarousel({
+        loop: true,
+        margin: 30,
+        mouseDrag: true,
+        autoplay: true,
+        dots: false,
+        nav: false,
+        navText: ["<span class='lnr ti-angle-left'></span>", "<span class='lnr ti-angle-right'></span>"],
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 2
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 4
+            }
+        }
+    });
+    //#endregion
+
+
+});
+
+
